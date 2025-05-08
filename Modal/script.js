c49f9cc326652f3e84ad0c3b1c06dd8d7ee3a5c0
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-08 13:08:20 +0500

    update

--- Modal/script.js
+++ Modal/script.js
@@ -1,24 +1,18 @@
-// Objectlarni jsga chaqirib olamiz
 const ShowModal = document.querySelectorAll('.showModal')
 const Modal = document.querySelector('.modal')
 const CloseBtn = document.querySelector('.closeBtn')
 const Overlay = document.querySelector('.overlay')
 
-// console.log(ShowModal, Modal, CloseBtn, Overlay);
 
-// "Hidden" classini uchiramiz
 const openModal=()=>{
     Modal.classList.remove('hidden')
     Overlay.classList.remove('hidden')
 }
-// "Hidden" classini qushamiz
 const closeModal=()=>{
     Modal.classList.add('hidden')
     Overlay.classList.add('hidden')
 }
-// overlay bosilganda modal yopiladi
 Overlay.addEventListener('click',closeModal)
-// close buttonni bosganda modal yopiladi
 CloseBtn.addEventListener('click',closeModal)
 
 // Button bosilganda modal ochiladi
@@ -26,7 +20,6 @@ for (let i = 0; i < ShowModal.length; i++){
     ShowModal[i].addEventListener('click',openModal)
 }
 
-// Enter va Escape klaviaturalarni bosganda modal ochilib yopiladi
 document.addEventListener('keydown',  (event)=>{
 if(event.key==='Enter'){
     openModal()
